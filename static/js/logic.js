8b26a0c55d6acde622a0c7500292c8bdb0402f19
Richard Tango <rjtango10@gmail.com>
2020-11-29 15:48:05 -0500

    again

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,104 +1,156 @@
-// Creating our initial map object
-// We set the longitude, latitude, and the starting zoom level for sf
-// This gets inserted into the div with an id of 'map' in index.html
-var myMap = L.map("map", {
-    center: [37.7749, -122.4194],
-    zoom: 5
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+
+// Maps
+var lightMap = L. tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?" +
+  "access_token=pk.eyJ1IjoidHNsaW5kbmVyIiwiYSI6ImNqaWNhdTFzdzFuam4za21sc3ZiMmN5bDEifQ.5Il8Y1QtwyMFWCa1JkDY_Q");
+
+var satMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}?" +
+  "access_token=pk.eyJ1IjoidHNsaW5kbmVyIiwiYSI6ImNqaWNhdTFzdzFuam4za21sc3ZiMmN5bDEifQ.5Il8Y1QtwyMFWCa1JkDY_Q");
+
+var streetmap = L.tileLayer(
+  "https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
+    "access_token=pk.eyJ1IjoidHNsaW5kbmVyIiwiYSI6ImNqaWNhdTFzdzFuam4za21sc3ZiMmN5bDEifQ.5Il8Y1QtwyMFWCa1JkDY_Q"
+);
+var darkmap = L.tileLayer(
+  "https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?" +
+    "access_token=pk.eyJ1IjoidHNsaW5kbmVyIiwiYSI6ImNqaWNhdTFzdzFuam4za21sc3ZiMmN5bDEifQ.5Il8Y1QtwyMFWCa1JkDY_Q"
+);
+
+
+function getColor(d) { 
+  if (d >= 5) { return "#FF0000" } else
+  if (d >= 4) { return "#FF6900" } else
+  if (d >= 3) { return "#FFC100" } else
+  if (d >= 2) { return "#E5FF00" } else
+  if (d >= 1) { return "#8DFF00" } else
+  if (d >= 0) { return "#00FF00" };
+};
+
+
+var earthquakes = new L.layerGroup();
+var timelineLayer = new L.layerGroup();
+
+
+var map = L.map("map", {
+    center: [
+      39.5, -98.35
+    ],
+    zoom: 3,
+    layers: [satMap, earthquakes]
+});
+
+
+d3.json(queryUrl, function(response) {
+
+  var getInterval = function(quake) {
+          return {
+            start: quake.properties.time,
+            end:   quake.properties.time + quake.properties.mag * 1800000
+          };
+        };
+
+  var timelineControl = L.timelineSliderControl({
+    formatOutput: function(date){
+      return moment(date).format("YYYY-MM-DD HH:MM:SS");
+    }
   });
-  
-  // Adding a tile layer (the background map image) to our map
-  // We use the addTo method to add objects to our map
-  L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "mapbox.streets",
-    accessToken: "pk.eyJ1IjoiY2hpYW15YzA5ODciLCJhIjoiY2swdzUxb3I2MGRiMzNpbnliN293OXBteiJ9.at8rk5Trv5oNH1dD2E9EAw"
-  }).addTo(myMap);
 
-  // Store our API endpoint
-// var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
+  // function styleData(feature) {
+  //     return {
+  //         stroke: true,
+  //         color: "black",
+  //         weight: .25,
+  //         fillOpacity: .7,
+  //         fillColor: getColor(feature.properties.mag),
+  //         radius: feature.properties.mag * 4
+  //     };
+  //   }
 
-//  GET color radius call to the query URL
-d3.json(queryUrl, function(data) {
-  function styleInfo(feature) {
+  var timeline = L.timeline(response, {
+
+    getInterval: getInterval,
+
+    pointToLayer: function(feature, latlng) {
+        return L.circle(latlng, {
+          stroke: true,
+          color: "black",
+          weight: .25,
+          fillOpacity: .7,
+          fillColor: getColor(feature.properties.mag),
+          radius: feature.properties.mag * 60000
+        }
+      )
+    },
+    // style: styleData,
+    onEachFeature: function(feature, layer) {
+        layer.bindPopup("<h3>" + feature.properties.place +
+    "</h3>" + new Date(feature.properties.time));
+    }
+  }).addTo(earthquakes);
+
+  earthquakes.addTo(map);
+
+        timelineControl.addTo(map);
+        timelineControl.addTimelines(timeline);
+        timeline.addTo(timelineLayer);
+        timelineLayer.addTo(map);
+
+})
+
+// Legend
+
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (myMap) {
+
+  var div = L.DomUtil.create('div', 'info legend'),
+      grades = [0, 1, 2, 3, 4, 5],
+      labels = [];
+
+  // loop through our density intervals and generate a label with a colored square for each interval
+  for (var i = 0; i < grades.length; i++) {
+      div.innerHTML +=
+          '<i style="background:' + getColor(grades[i]) + '"></i> ' +
+          grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+  }
+  return div;
+};
+
+legend.addTo(map);
+
+
+
+var faults = new L.layerGroup();
+
+faultsURL = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
+
+d3.json(faultsURL, function(response) {
+  function faultStyle(feature) {
     return {
-      opacity: 1,
-      fillOpacity: 1,
-      fillColor: getColor(feature.properties.mag),
-      color: "#000000",
-      radius: getRadius(feature.properties.mag),
-      stroke: true,
-      weight: 0.5
+      weight: 2,
+      color: "orange"
     };
   }
-  // set different color from magnitude
-    function getColor(magnitude) {
-    switch (true) {
-    case magnitude > 5:
-      return "#ea2c2c";
-    case magnitude > 4:
-      return "#ea822c";
-    case magnitude > 3:
-      return "#ee9c00";
-    case magnitude > 2:
-      return "#eecc00";
-    case magnitude > 1:
-      return "#d4ee00";
-    default:
-      return "#98ee00";
-    }
-  }
-  // set radiuss from magnitude
-    function getRadius(magnitude) {
-    if (magnitude === 0) {
-      return 1;
-    }
 
-    return magnitude * 4;
-  }
-    // GeoJSON layer
-    L.geoJson(data, {
-      // Maken cricles
-      pointToLayer: function(feature, latlng) {
-        return L.circleMarker(latlng);
-      },
-      // cirecle style
-      style: styleInfo,
-      // popup for each marker
-      onEachFeature: function(feature, layer) {
-        layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
-      }
-    }).addTo(myMap);
-  
-    // an object legend
-    var legend = L.control({
-      position: "bottomright"
-    });
-  
-    // details for the legend
-    legend.onAdd = function() {
-      var div = L.DomUtil.create("div", "info legend");
-  
-      var grades = [0, 1, 2, 3, 4, 5];
-      var colors = [
-        "#98ee00",
-        "#d4ee00",
-        "#eecc00",
-        "#ee9c00",
-        "#ea822c",
-        "#ea2c2c"
-      ];
-  
-      // Looping through
-      for (var i = 0; i < grades.length; i++) {
-        div.innerHTML +=
-          "<i style='background: " + colors[i] + "'></i> " +
-          grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
-      }
-      return div;
-    };
-  
-    // Finally, we our legend to the map.
-    legend.addTo(myMap);
-  });
+  L.geoJSON(response, {
+    style: faultStyle
+  }).addTo(faults);
+  faults.addTo(map)
+})
+
+
+
+var overlayMaps = {
+    "Earthquakes": earthquakes,
+    "Fault lines": faults
+};
+var baseMaps = {
+    "Light map": lightMap,
+    "Dark map": darkmap,
+    "Street map": streetmap,
+    "Satellite": satMap    
+};
+
+L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+}).addTo(map);
